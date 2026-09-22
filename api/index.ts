61fa0766e4b189fd28c1ca673a7549bae467d749
meesalavamsi <meesalavamsi@users.noreply.github.com>
2026-09-22 18:14:21 +0530

    Add Vercel configuration

---
+++ api/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import { registerRoutes } from '../server/routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+registerRoutes(app);
+
+export default app;
